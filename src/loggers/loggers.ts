73bfcc39da1bf0be76c6aa2bf04a999640a5098c
Luke Elliot <luke@blendedsense.com>
2024-10-18 16:41:06 -0400

    Adds loggers/loggers.ts: Adds winston loggers for sequelize and products

---
+++ src/loggers/loggers.ts
@@ -0,0 +1,38 @@
+import winston from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+
+const logFormat = winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+)
+
+const sequelizeLogger = winston.createLogger({
+    level: process.env.LOG_LEVEL || 'info',
+    format: logFormat,
+    transports: [
+        new DailyRotateFile({
+            filename: 'sequelize-%DATE%.log',
+            dirname: './logs',
+            datePattern: 'YYYY-MM-DD',
+            maxFiles: '7d',
+        }),
+    ],
+})
+
+const productsLogger = winston.createLogger({
+    level: process.env.LOG_LEVEL || 'info',
+    format: logFormat,
+    transports: [
+        new DailyRotateFile({
+            filename: 'products-%DATE%.log',
+            dirname: './logs',
+            datePattern: 'YYYY-MM-DD',
+            maxFiles: '7d',
+        }),
+    ],
+})
+
+export {
+    sequelizeLogger,
+    productsLogger,
+}
