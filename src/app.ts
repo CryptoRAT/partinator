7c58a0d8b1969e25fd0902b4b8bbe48178c58d97
Luke Elliot <luke@blendedsense.com>
2024-10-18 16:47:23 -0400

    Adds loadEnv.ts: Handles loading the environment variables. Adds loadEnv.test.ts: tests the new module Modifies app.ts: to use loadEnv

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import loadRoutes from './loadRoutes';
 import path from 'path';
 const app = express();
+import './loadEnv';
 
 // Load routes
 const routesPath = path.join(__dirname, 'routes');
