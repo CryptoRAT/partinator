6aabbae2c0ba600bd9d719291ed2101e10f7d963
Luke Elliot <luke@blendedsense.com>
2024-10-18 16:45:01 -0400

    Modifies jest.config.ts: Adds moduleNameMapper so the tests can find the files without having to do relative values everywhere.

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    moduleFileExtensions: ['ts', 'js'],
+    moduleFileExtensions: ['ts','js'],
     testMatch: ['**/__test__/**/*.test.ts'],
     transform: {
         '^.+\\.ts$': 'ts-jest',
@@ -16,6 +16,14 @@ const config: Config = {
             lines: 90,
             statements: 90,
         },
+    },
+    moduleNameMapper: {
+        '^@db/(.*)$': '<rootDir>/src/datasources/$1',
+        '^@admin/(.*)$': '<rootDir>/src/admin/$1',
+        '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+        '^@models/(.*)$': '<rootDir>/src/models/$1',
+        '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+        '^@loggers/(.*)$': '<rootDir>/src/loggers/$1',
     },
 };
 
