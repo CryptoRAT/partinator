66764aad2467ec9866d461927b2112d33a820381
Luke Elliot <luke@blendedsense.com>
2024-10-20 08:22:37 -0400

    Adds config/swaggerConfig.ts. Modifies routes/product.ts and app.ts: To support and use swagger. Modifies yarn.lock: Updated to include new swagger libs and versions needed to support ES modules.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import loadRoutes from './loadRoutes';
 import path from 'path';
-const app = express();
+
 import './loadEnv';
+import swaggerUi from "swagger-ui-express";
+import {swaggerSpec} from "@config/swaggerConfig";
+
+const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Load routes
 const routesPath = path.join(__dirname, 'routes');
