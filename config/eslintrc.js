4425fb74e0dbdc911ccd94ba71033534bca1f4f0
Luke Elliot <luke@blendedsense.com>
2024-10-22 03:53:25 -0400

    moved the eslint file and made it javascript

---
+++ config/eslintrc.js
@@ -0,0 +1,22 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+    ignorePatterns: ['build/', 'node_modules/'],
+};
