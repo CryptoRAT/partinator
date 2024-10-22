db1350f8b38d7f74065e23bc4f6604388582d7a5
Luke Elliot <luke@blendedsense.com>
2024-10-22 06:09:08 -0400

    initial add of adminjs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from './app';
 import {isMainModule} from "@utils/instanceUtils.ts";
+import path from "path";
+import fs from "fs";
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,5 +12,8 @@ const startServer = (port = PORT) => {
 };
 
 if(isMainModule(module)) {
+    const designSystemPath = path.resolve(__dirname, 'node_modules', '@adminjs', 'design-system', 'package.json');
+    const pkgContent = JSON.parse(fs.readFileSync(designSystemPath, 'utf8'));
+    console.log(pkgContent.exports);
     startServer();
 }
